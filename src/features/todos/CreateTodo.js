171006a3696fb984d461c1ec44f13f0f35f27969
William Luck <luck.william.h@gmail.com>
2023-03-25 14:56:55 -0300

    commit tree 97aa276a60f68cf6a68722c359e6f9b9078b136f

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { todoAdded } from "./todosSlice";
+
 
 function CreateTodo() {
-  return <div>Create Todo Component</div>;
+
+  const dispatch = useDispatch()
+  const [todo, setTodo] = useState('')
+  const todos = useSelector(state => state.entities)
+
+  function handleChange(e) {
+    setTodo(e.target.value)
+  }
+
+  function handleSubmit(e){ 
+    e.preventDefault()
+    dispatch(todoAdded(todo))
+    setTodo('')
+  }
+  return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <p>
+            <label>add todo</label>
+            <input type="text" onChange={handleChange} value={todo}/>
+          </p>
+          <input type="submit" />
+        </form>
+
+        {todos?.map(todo => todo)}
+      </div>
+
+      
+  )
 }
 
 export default CreateTodo;
